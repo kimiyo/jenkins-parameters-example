c8c41ff147eab4cf0fa4d7f959d9ebc1a91bedca
Jong Hoon Kim <kimiyo@naver.com>
2018-06-24 21:44:22 +0900

    add run-args.sh

---
+++ run-args.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+let "cnt=1"
+for arg in "$@"
+do
+  let "cnt+=1"
+  echo argument $cnt: $arg
+done
+
+#env=prod
+if [[  "$env" =~ ^(dev|st|sit|uat|preprod|prod) ]]; then
+   echo "$env is in the environment list"
+else
+   echo "$env is not in the environment list"
+fi
